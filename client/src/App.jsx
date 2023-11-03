790a5d2d3b5b57a4860d2953582da984c850a336
XH0PE <pecioso999@gmail.com>
2023-11-03 08:33:46 -0500

    109

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+export default function App() {
+  return (
+    <div> <h1> Bienvenidos !!</h1>
+    <div>
+    <Button primary>Primary</Button>
+    <Button secondary>Secondary</Button>
+    </div>
+    </div>
+  )
+}
